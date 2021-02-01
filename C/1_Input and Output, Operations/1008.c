8f1765d3bc92ad9229e96bc5a0b4f6e07107ac24
wonlody <70581972+wonlody@users.noreply.github.com>
2021-02-01 21:00:22 +0900

    Create 1008.c

---
+++ C/1_Input and Output, Operations/1008.c
@@ -0,0 +1,17 @@
+/*
+   problem 1008 : A/B
+   BACKJOON >> baekjoon, djm03178, jh05013
+   
+   두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    double a, b;
+    scanf("%lf %lf", &a, &b);
+    printf("%.9lf", a/b);
+    
+    return 0;
+}
