76f07333e73cfe3020d5d3df89a4f7d2a00ada58
wonlody <70581972+wonlody@users.noreply.github.com>
2021-02-01 21:07:19 +0900

    Create 10869.c

---
+++ C/1_Input and Output, Operations/10869.c
@@ -0,0 +1,21 @@
+/*
+   problem 10869 : 사칙연산
+   BACKJOON
+   
+   두 자연수 A와 B를 입력받아, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    long a, b;
+    scanf("%ld %ld", &a, &b);
+    printf("%ld\n", a+b);
+    printf("%ld\n", a-b);
+    printf("%ld\n", a*b);
+    printf("%ld\n", a/b);
+    printf("%ld\n", a%b);
+    
+    return 0;
+}
