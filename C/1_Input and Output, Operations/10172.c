a50ac48e95a3b4efa353a1f420823e64885607f1
wonlody <70581972+wonlody@users.noreply.github.com>
2021-02-01 20:43:38 +0900

    Create 10172.c

---
+++ C/1_Input and Output, Operations/10172.c
@@ -0,0 +1,19 @@
+/*
+   problem 10172 : 개
+   BACKJOON >> High School > PLU High School Programming Contest > PLU 2014 - Novice 3번
+   
+   key point : 역슬래시(\), 큰따옴표("") 출력
+*/
+
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("|\\_/|\n");
+    printf("|q p|   /}\n");
+    printf("( 0 )\"\"\"\\\n");
+    printf("|\"^\"`    |\n");
+    printf("||_/=\\\\__|\n");
+    return 0;
+}
