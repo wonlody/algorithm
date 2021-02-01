05bc26760989a62abd49ef917b7b1ed3605633df
wonlody <70581972+wonlody@users.noreply.github.com>
2021-02-01 20:58:14 +0900

    Create 10998.c

---
+++ C/1_Input and Output, Operations/10998.c
@@ -0,0 +1,17 @@
+/*
+   problem 10998 : A×B
+   BACKJOON >> baekjoon
+   
+   두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a*b);
+    
+    return 0;
+}
