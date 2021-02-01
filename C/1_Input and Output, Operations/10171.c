6d9fc5e8b695d9c4fcf4c5614bf0dfe6180f2531
wonlody <70581972+wonlody@users.noreply.github.com>
2021-02-01 20:43:17 +0900

    Create 10171.c

---
+++ C/1_Input and Output, Operations/10171.c
@@ -0,0 +1,18 @@
+/*
+   problem 10171 : 고양이
+   BACKJOON >> High School > PLU High School Programming Contest > PLU 2014 - Novice 2번
+   
+   key point : 역슬래시(\) 출력
+*/
+
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("\\    /\\\n");
+    printf(" )  ( ')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|\n");
+    return 0;
+}
