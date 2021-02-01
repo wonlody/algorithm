ea01016af5e5a187d520efdb8bdfef15578afb50
wonlody <70581972+wonlody@users.noreply.github.com>
2021-02-01 20:42:38 +0900

    Create 10718.c

---
+++ C/1_Input and Output, Operations/10718.c
@@ -0,0 +1,17 @@
+/*
+   problem 10718 : We love kriii
+   BAEKJOON >> Contest > Coder's High > Coder's High 2015 Side Contest P1번
+   tae
+   
+   key point : 줄바꿈(\n)
+*/
+
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("강한친구 대한육군\n");
+    printf("강한친구 대한육군\n");
+    return 0;
+}
