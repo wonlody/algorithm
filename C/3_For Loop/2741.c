fb85dd12d128230653fc5c4d2845135e1aceb4b7
wonlody <70581972+wonlody@users.noreply.github.com>
2021-02-02 22:08:44 +0900

    Create 2741.c

---
+++ C/3_For Loop/2741.c
@@ -0,0 +1,21 @@
+/*
+   problem 2741 : N찍기
+   BAEKJOON >> baekjoon, rory143
+   
+   자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    long n;
+    scanf("%ld", &n);
+
+    for (int i = 1; i < n+1; i++)
+    {
+        printf("%d\n", i);
+    }
+    
+    return 0;
+}
