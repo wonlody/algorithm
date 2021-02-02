66850f6c1d2bf544803067993081a129ef11579b
wonlody <70581972+wonlody@users.noreply.github.com>
2021-02-02 22:15:15 +0900

    Create 11021.c

---
+++ C/3_For Loop/11021.c
@@ -0,0 +1,21 @@
+/*
+   problem 11021 : A+B-7
+   BAEKJOON >> baekjoon, djm03178
+   
+   두 정수 A,B를 입력받아, A+B를 출력하는 프로그램
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int t, a, b;
+    scanf("%d", &t);
+
+    for (int i = 1; i < n+1; i++)
+    {
+        printf("%d\n", i);
+    }
+    
+    return 0;
+}
