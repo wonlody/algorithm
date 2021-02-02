a69ba26f287725d587ae91bae1af505eaeb7c453
wonlody <70581972+wonlody@users.noreply.github.com>
2021-02-02 16:37:28 +0900

    Create 14681.c

---
+++ C/2_If Statement/14681.c
@@ -0,0 +1,41 @@
+/*
+   problem 14681 : 사분면 고르기
+   BACKJOON >> Olympiad > Canadian Computing Competition & Olympiad > 2017 > CCC 2017 Junior Division 1번
+   jh05013
+   
+   주어진 점(x, y)이 어느 사분면에 있는지 출력하는 프로그램
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int x, y;
+    scanf("%d", &x);
+    scanf("%d", &y);
+    
+    if (x > 0)
+    {
+      if (y > 0)
+      {
+        printf("1\n");
+      }
+      else
+      {
+        printf("4\n");
+      }
+    }
+    else
+    {
+      if (y > 0)
+      {
+        printf("2\n");
+      }
+      else
+      {
+        printf("3\n");
+      }
+    }
+   
+    return 0;
+}
