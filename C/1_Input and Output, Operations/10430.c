9a9eec139e1fc5cf166c2357674d07c52aff0c7e
wonlody <70581972+wonlody@users.noreply.github.com>
2021-02-02 18:10:38 +0900

    Update 10430.c

--- C/1_Input and Output, Operations/10430.c
+++ C/1_Input and Output, Operations/10430.c
@@ -1,6 +1,6 @@
 /*
    problem 10869 : 사칙연산
-   BACKJOON >> baekjoon, eric00513
+   BAEKJOON >> baekjoon, eric00513
    
    세 수 A, B, C를 입력받아, (A+B)%C, ((A%C) + (B%C))%C, (A×B)%C, ((A%C) × (B%C))%C를 출력하는 프로그램
 */
