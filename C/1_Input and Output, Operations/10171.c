09854d77ed5470868b26719b6f193ebf2c8190b2
wonlody <70581972+wonlody@users.noreply.github.com>
2021-02-02 18:08:59 +0900

    Update 10171.c

--- C/1_Input and Output, Operations/10171.c
+++ C/1_Input and Output, Operations/10171.c
@@ -1,6 +1,6 @@
 /*
    problem 10171 : 고양이
-   BACKJOON >> High School > PLU High School Programming Contest > PLU 2014 - Novice 2번
+   BAEKJOON >> High School > PLU High School Programming Contest > PLU 2014 - Novice 2번
    
    key point : 역슬래시(\) 출력
 */
