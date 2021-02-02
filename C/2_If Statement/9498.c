cac81014243fe019a20168771da63babb42f7dc1
wonlody <70581972+wonlody@users.noreply.github.com>
2021-02-02 15:55:08 +0900

    Create 9498.c

---
+++ C/2_If Statement/9498.c
@@ -0,0 +1,37 @@
+/*
+   problem 9498 : 시험 성적
+   BACKJOON >> baekjoon
+   
+   시험 점수를 입력받아, 시험 성적{A(90~100), B(80~89), C(70~79), D(60~69), F(나머지)}을 출력하는 프로그램
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    scanf("%d", &a);
+    
+    if (90 <= a && a <= 100)
+    {
+      printf("A\n");
+    }
+    else if (80 <= a)
+    {
+      printf("B\n");
+    }
+    else if (70 <= a)
+    {
+      printf("C\n");
+    }
+    else if (60 <= a)
+    {
+      printf("D\n");
+    }
+    else
+    {
+      printf("F\n");
+    }
+   
+    return 0;
+}
