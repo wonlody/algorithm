8b658d7114b9c5314c9b29707b1dd5cb9e285767
wonlody <70581972+wonlody@users.noreply.github.com>
2021-02-02 23:22:22 +0900

    Create 2438.c

---
+++ C/3_For Loop/2438.c
@@ -0,0 +1,25 @@
+/*
+   problem 2438 : 별 찍기-1
+   BAEKJOON >> baekjoon, grace0068, hchanhong
+   
+   첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i < n+1; i++)
+    {
+        for (int j = 1; j < i+1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
