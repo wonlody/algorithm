2e457930dab56968bc964b7da58b7af572d04f0f
wonlody <70581972+wonlody@users.noreply.github.com>
2021-02-02 15:44:09 +0900

    Create 1330.c

---
+++ C/2_If Statement/1330.c
@@ -0,0 +1,29 @@
+/*
+   problem 1330 : 두 수 비교하기
+   BACKJOON
+   
+   두 정수 A, B를 입력받아, A와 B를 비교하는 프로그램
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    if (a > b)
+    {
+      printf(">\n");
+    }
+    else if (a < b)
+    {
+      printf("<\n");
+    }
+    else
+    {
+      printf("==\n");
+    }
+   
+    return 0;
+}
