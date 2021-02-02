205e2765829c0577df1eba86be9511e63cd001d2
wonlody <70581972+wonlody@users.noreply.github.com>
2021-02-02 18:19:29 +0900

    Update 2739.c

--- C/3_For Loop/2739.c
+++ C/3_For Loop/2739.c
@@ -2,30 +2,19 @@
    problem 2739 : 구구단
    BAEKJOON
    
-   알람 시각이 주어졌을 때, 45분 일찍 시간을 바꾸는 프로그램
+   N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램
 */
 
 #include <stdio.h>
 
 int main(void)
 {
-    int h, m;
-    scanf("%d %d", &h, &m);
+    int n;
+    scanf("%d", &n);
     
-    if (m >= 45)
+    for (int i = 1; i < 10; i++)
     {
-      printf("%d %d\n", h, m-45);
-    }
-    else
-    {
-      if (h != 0)
-      {
-        printf("%d %d\n", h-1, m+15);
-      }
-      else
-      {
-        printf("%d %d\n", h+23, m+15);
-      }
+       printf("%d * %d = %d\n", n, i, n*i);
     }
    
     return 0;
