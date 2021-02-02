e8c77919f153c646c118dcaa1676e73557de3b83
wonlody <70581972+wonlody@users.noreply.github.com>
2021-02-02 23:38:21 +0900

    Update 2438.c

--- C/3_For Loop/2438.c
+++ C/3_For Loop/2438.c
@@ -2,7 +2,7 @@
    problem 2438 : 별 찍기-1
    BAEKJOON >> baekjoon, grace0068, hchanhong
    
-   첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+   첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 프로그램
 */
 
 #include <stdio.h>
